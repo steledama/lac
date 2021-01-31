9dabf143385763a1f44db167e16b458c59b27017
steledama <51272513+steledama@users.noreply.github.com>
2021-01-31 01:11:33 +0100

    SIMPLE no percent calculation
    percent and boolean calculation passed to zabbix server template

--- agent.js
+++ agent.js
@@ -1,30 +1,32 @@
 const snmp = require("net-snmp");
 const { sendZabbix} = require ('./sendZabbix');
-const templates = require("./templates.json");
+const printersTemplates = require("./printersTemplates.json");
 const monitoredPrinters = require('./monitoredPrinters.json');
 const serverZabbix = 'print.clasrl.com'
 
 //for each printer to monitored (taken from monitoredPrinters.json)...
 monitoredPrinters.forEach(printer => {
-    //take from tamplates.json oids and names arrays and add to printers
-    printer ["items"] = templates[printer.brand][printer.family][printer.model];
-    printer ["oids"] = [];
-    for (item in printer.items){
-        printer.oids.push(item)
+    //take from tamplates.json oids and names and add to printers
+    printer ["oids"] = printersTemplates[printer.brand][printer.family][printer.model];
+
+    //take onlyoid and put in array
+    printer ["oidsArray"] = [];
+    for (oid in printer.oids){
+        printer.oidsArray.push(oid)
     }
+
+    //prepare items object to send
+    printer["items"]={};
+
     //snmp session to retrive oids status
     let session = snmp.createSession(printer.ip);
-    session.get(printer.oids, function (error, varbinds) {
+    session.get(printer.oidsArray, function (error, varbinds) {
         if (error) {
             console.error(error);
         } else {
             for (let i = 0; i < varbinds.length; i++)
-                if (snmp.isVarbindError(varbinds[i]))
-                    console.error(snmp.varbindError(varbinds[i]));
-
-                else
-                    printer.items[printer.oids[i]]["itemStatus"] = (varbinds[i].value);
-                    //console.log(`${varbinds[i].oid} = ${varbinds[i].value}`);
+                if (snmp.isVarbindError(varbinds[i])) console.error(snmp.varbindError(varbinds[i]));
+                else printer.items[printer.oids[varbinds[i].oid]] = varbinds[i].value;
         }
         session.close();
     });
@@ -32,9 +34,8 @@ monitoredPrinters.forEach(printer => {
         if (error)
             console.error(error);
     });
-    setTimeout(() => {  console.log(printer); }, 10000);
 
-    // calculate data in percentage or boolean if needed..
+    setTimeout(() => {  console.log(printer); }, 10000);
 
     //send results to zabbix server
     //sendZabbix(server, model, serial, names, status)
