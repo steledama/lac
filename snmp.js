0f58eff5db41e78699bd0879bcc1bf7b3de085ea
stefano <stefano.pompa@gmail.com>
2021-05-17 20:12:27 +0200

    snmp cli fixed

--- snmp.js
+++ snmp.js
@@ -19,7 +19,7 @@ return new Promise((resolve,reject) => {
                     };
                     final_result.push(snmp_rez);
                 }
-            console.log(final_result);
+            //console.log(final_result);
             resolve(final_result)
         }
     });
@@ -53,7 +53,7 @@ const subtree = (ip,oid) => {
             if (error) { 
                 reject(error);
             } else {
-                console.log(final_result);
+                //console.log(final_result);
                 resolve(final_result);
             }
         });
@@ -70,44 +70,25 @@ if (process.argv[2] != undefined) {
         else {
             let passedMethod = process.argv[3];
             if (passedMethod == 'subtree') {
-                if (process.argv[4] = null) {
-                    let passedOid = "1.3.6.1.2.1.43.5.1.1.17"; // SUBTREE default
-                    console.log(passedIp,passedOid);
-                    subtreeRequest(passedIp, passedOid)
-                }else {
-                    let passedOid = process.argv[4];
-                    try {
-                        subtree(passedIp, passedOid)
-                    }
-                    catch {
-                        // error
-                    }
-                }
-                async function subtreeRequest() {
+                let passedOid = (process.argv[4] || "1.3.6.1.2.1.43.5.1.1.17");// SUBTREE default
+                subtreeRequest(passedIp, passedOid)
+
+                async function subtreeRequest(ip,oid) {
                     let result = await subtree(ip, oid);
                     console.log(result);
                 };
             } else {
-                if (passedOid = null) {
-                    let oidsArray = ["1.3.6.1.2.1.1.5.0"]; // GET default
-                    getRequest(passedIp, oidsArray);
-                }
-                else {
-                    let oidsArray = [passedOid];
-                    getRequest(passedIp, oidsArray);
-                }
+                let passedOid = (process.argv[4] || "1.3.6.1.2.1.1.5.0");// GET default
+                let oidsArray = [passedOid];
+                getRequest(passedIp, oidsArray);
+
                 async function getRequest (ip, array) {
                     let result = await get(ip, array);
                     console.log(result);
                 }
-                
             }
         }
     } else console.log ('You need to pass a method after the ip');
 }
-
-//get('192.168.1.66',['1.3.6.1.2.1.1.5.0']);
-//get("192.168.1.66", ["1.3.6.1.2.1.43.5.1.1.17.1"]);
-//subtree("192.168.1.66", "1.3.6.1.2.1.43.5.1.1.17");
 
 module.exports = {get, subtree};
