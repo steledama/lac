398339ffec198a4945a9ebf4cf003bd58dfd4db2
stefano <stefano.pompa@gmail.com>
2021-05-17 22:10:51 +0200

    snmp cli naming

--- snmp.js
+++ snmp.js
@@ -1,6 +1,6 @@
 // To comunicate with snmp devices
 const snmp = require ("net-snmp");
-//const fs = require ("fs");
+const fs = require ("fs");
 
 const get = (ip,oidsArray) => {
 return new Promise((resolve,reject) => {
@@ -68,17 +68,41 @@ if (process.argv[2] != undefined) {
             console.log('The method must be get or subtree');
         }
         else {
-            let passedMethod = process.argv[3];
-            if (passedMethod == 'subtree') {
-                let passedOid = (process.argv[4] || "1.3.6.1.2.1.43.5.1.1.17");// SUBTREE default
+            if (process.argv[3] == 'subtree') {
+                let passedOid = "";
+                switch (process.argv[4]) {
+                    case 'usage':
+                        passedOid = "1.3.6.1.4.1.253.8.53.13.2.1.6.1.20";
+                        break;
+                    case 'supply':
+                        passedOid = "1.3.6.1.2.1.43.11.1.1";
+                        break;
+                    default:
+                        passedOid = "1.3.6.1";
+                }
                 subtreeRequest(passedIp, passedOid)
-
+                // SUBTREE default
                 async function subtreeRequest(ip,oid) {
                     let result = await subtree(ip, oid);
-                    console.log(result);
+                    if (oid != '1.3.6.1') console.log(result);
+                    else try {
+                        const data = fs.writeFileSync('snmp.txt', JSON.stringify(result, null, 4))
+                      } catch (err) {
+                        console.error(err)
+                      }
                 };
             } else {
-                let passedOid = (process.argv[4] || "1.3.6.1.2.1.1.5.0");// GET default
+                let passedOid = "";
+                switch (process.argv[4]) {
+                    case 'serial':
+                        passedOid = "1.3.6.1.2.1.43.5.1.1.17.1";
+                        break;
+                    case 'name':
+                        passedOid = "1.3.6.1.2.1.1.5.0";
+                        break;
+                    default:
+                        passedOid = "1.3.6.1.2.1.1.1.0";
+                }
                 let oidsArray = [passedOid];
                 getRequest(passedIp, oidsArray);
 
