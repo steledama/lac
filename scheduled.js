6dd78314cdcd22ef49327e01b061327611d864ae
stefano <stefano.pompa@gmail.com>
2021-05-29 00:31:03 +0200

    scheduled snmpResults

--- scheduled.js
+++ scheduled.js
@@ -5,108 +5,130 @@ const { resolve } = require("path");
 // built in node module for os information
 const os = require('os');
 // for snmp comunication from snmp.js
-// app.mjs
-import { lacGet } from './snmp.js';
+const lac = require ('./snmp.js');
+// to make http request
+const axios = require('axios').default;
 
 // version taken from package.json
 const package = require('./package.json');
 let version = package.version;
-console.log(version);
+//console.log(version);
 
 // zabbix server
-const serverZabbix = 'stele.dynv6.net';
+const nameOid = '1.3.6.1.2.1.1.1.0';
+const serialOid = '1.3.6.1.2.1.43.5.1.1.17.1';
 
-let devices = ["192.168.1.66"]
-let deviceName = "Xerox Phaser 6130N"
-/* const lacGet = (ip,oidsArray) => {
-    return new Promise((resolve,reject) => {
-        final_result = [];
-        let session = snmp.createSession(ip);
-        session.get(oidsArray, function (error, varbinds) {
-            if (error) { 
-                reject(error);
-            } else {
-                for (let i = 0; i < varbinds.length; i++)
-                    if (snmp.isVarbindError(varbinds[i])) reject(snmp.varbindError(varbinds[i]));
-                    else {
-                        let snmp_rez = {
-                          oid: (varbinds[i].oid).toString(),
-                          value: (varbinds[i].value).toString()
-                        };
-                        final_result.push(snmp_rez);
-                    }
-                //console.log(final_result);
-                resolve(final_result)
-            }
-        });
-        session.trap(snmp.TrapType.LinkDown, function (error) {
-            if (error) reject(error);
-        });
-    });
-  }; */
+let serverZabbix = 'stele.dynv6.net';
+let devices = [
+    {
+        ip: "192.168.1.3",
+        client: "STEFANO",
+        site: "",
+        location: ""
+    }
+];
+//let deviceName = "Xerox Phaser 6130N";
 
 //for each printer to monitor (taken from lac server)
-devices.forEach(device => {
-    //find the printer template from zabbix server
-    let deviceTemplate = async (deviceName) => {
-        try {
-          const response = await axios.post(`http://${serverZabbix}/api_jsonrpc.php`, {
+devices.forEach(async device => {
+
+    // connect to device to get sys name
+    let deviceNameUncleaned = (await lac.get(device.ip,[nameOid]))[0].value;
+    // clean device name
+    device.name = (cleanName(deviceNameUncleaned))[0];
+    //console.log(device);
+
+    // connect to device to get serial number
+    device.serial = (await lac.get(device.ip,[serialOid]))[0].value;
+    //console.log(device);
+
+    // connect to zabbix server to check if the host exist
+    device.hostId = await getHost(device.serial);
+    //console.log(device);
+
+    //find the printer template id from zabbix server
+    device.templateId = await getDeviceTemplateId(device.name);
+    //console.log(device);
+
+    // find device items from zabbix server
+    device.items = await getDeviceItems(device.templateId);
+    //console.log(device);
+
+    //take only oids and put them into oidsArray
+    let oidsArray = [];
+    device.items.forEach(item => {
+        oidsArray.push(item.key_);
+    });
+    //console.log(oidsArray);
+
+    // connect to device and get oids values
+    let snmpResults = await lac.get(device.ip,oidsArray)
+    console.log(snmpResults);
+});
+
+function cleanName(string) {
+    let res = string.split(";", 1);
+    return res;
+  }
+
+// connect with zabbix server to get device template id from device name
+async function getDeviceTemplateId (deviceName) {
+    try {
+        const response = await axios.post(`http://${serverZabbix}/api_jsonrpc.php`, {
+        "jsonrpc": "2.0",
+        "method": "template.get",
+        "params": {
+            "output": ["host","templateid"],
+            "filter": {"host": [`${deviceName}`]}
+        },
+        "auth": "0eb8340a0cee7c02c24f50b4cb322d03",
+        "id": 1
+        });
+        //console.log(response.data);
+        return response.data.result[0].templateid;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// connect to zabbix server to get device items from template id
+async function getDeviceItems (templateId) {
+    try {
+        const response = await axios.post(`http://${serverZabbix}/api_jsonrpc.php`, {
+        "jsonrpc": "2.0",
+        "method": "item.get",
+        "params": {
+            "output": ["name","key_"],
+            "templateids": `${templateId}`,
+            "tags": [{"tag": "false", "operator": "2"}]
+        },
+        "auth": "0eb8340a0cee7c02c24f50b4cb322d03",
+        "id": 1
+        });
+        //console.log(response.data);
+        return response.data.result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// connect to zabbix server to get host from device serial number
+async function getHost (deviceSerial) {
+    try {
+        const response = await axios.post(`http://${serverZabbix}/api_jsonrpc.php`, {
             "jsonrpc": "2.0",
-            "method": "template.get",
+            "method": "host.get",
             "params": {
-                "output": ["host","templateid"],
-                "filter": {
-                    "host": [
-                        `${deviceName}`
-                    ]
-                }
+                "filter": { "host": [`${deviceSerial}`] }
             },
             "auth": "0eb8340a0cee7c02c24f50b4cb322d03",
             "id": 1
-          });
-          console.log(deviceTemplate);
-          //return response
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    // take the oids (name and oid)
-    printer["oids"] = printerTemplate.oids;
-    //take only oid and put in oidsArray
-    printer ["oidsArray"] = [];
-    for (oid in printer.oids) printer.oidsArray.push(oid);
-    //call the snmpGet function to take oids values from printer
-    //console.log(printer);
-    let getOidsValues = async () => {
-        let snmpResults = await lacGet(printer.ip,printer.oidsArray);
-        snmpResults.forEach(result =>{
-            printer.oids[result.oid]["value"] = result.value;
-        })
-        //console.log(printer);
-        printer.toSend ={};
-        printer.items =Object.values(printer.oids);
-        calcValues(printer);
-        printer.toSend ={};
-        printer.items =Object.values(printer.oids);
-        calcValues(printer);
-        sendZabbix(printer);
+        });
+        //console.log(response.data);
+        return response.data.result;
+    } catch (error) {
+        console.error(error);
     }
-    getOidsValues(); 
-});
-
-function calcValues(printer) {
-    // Prepare and calculate items to send
-    printer.items.forEach(item => {
-        if (item.type === "asIs") printer.toSend[item.name] = item.value;
-        if (item.type === "total") {
-            let itemRemain = printer.items.find(itemToFind => item.name === itemToFind.name && itemToFind.type === "remain");
-            printer.toSend[item.name] = Math.round(itemRemain.value/item.value*100);
-        }
-        if (item.type === "boolean") {
-            if (item.value === item.isFalse)printer.toSend[item.name] = "OK";
-            else printer.toSend[item.name] = true;
-        }
-    })
 }
 
 //function to send results to zabbix server
