192075183979bd50812d7becbcbd1a82d1138f3c
steledama <stefano.pompa@gmail.com>
2021-08-30 07:21:01 +0200

    save form ui

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,24 @@
-// import fs from 'fs';
+import fs from 'fs';
 import React from 'react';
 import Config from './components/Config';
 import { useState } from 'react';
 
-// export const getServerSideProps = async () => {
-//   try {
-//     const data = fs.readFileSync('config.json', 'utf8');
-//     const config = JSON.parse(data);
-//     return {
-//       props: {
-//         config,
-//       },
-//     };
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
+export const getServerSideProps = async () => {
+  try {
+    const data = fs.readFileSync('config.json', 'utf8');
+    const configFromFile = JSON.parse(data);
+    return {
+      props: {
+        configFromFile,
+      },
+    };
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-export default function Home() {
-  const [config, setConfig] = useState({
-    zabbixServer: 'stele.dynv6.net',
-    zabbixToken: 'slòdkjhsòoldgfj',
-    zabbixGroup: 'STE',
-    agentLocation: 'casa Stefano',
-  });
+export default function Home({ configFromFile }) {
+  const [config, setConfig] = useState(configFromFile);
 
   // save config
   const onSave = (config) => {
