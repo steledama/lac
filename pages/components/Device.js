e69da4912bade82dacc66bfe890eaa4eb8b38bfb
steledama <stefano.pompa@gmail.com>
2021-09-26 11:56:50 +0200

    delete device with confirm modal

--- pages/components/Device.js
+++ pages/components/Device.js
@@ -1,15 +1,24 @@
 import { useState, useEffect } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Modal } from 'react-bootstrap';
 import axios from 'axios';
 import Feedback from './Feedback';
 
 const Device = ({ conf, device, onDelete, onStop }) => {
   const [deviceMessage, setDeviceMessage] = useState({});
 
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const deviceIp = device.tags.find((el) => el.tag === 'deviceIp');
   const latestUrl = `http://${conf.server}/zabbix.php?action=latest.view&filter_hostids%5B%5D=${device.hostid}&filter_set=1`;
   const configUrl = `http://${conf.server}/hosts.php?form=update&hostid=${device.hostid}`;
   const deviceUrl = `http://${deviceIp.value}`;
+
+  // monitor device at start
+  useEffect(() => {
+    monitorDevice();
+  }, []);
 
   // monitor device
   const monitorDevice = async () => {
@@ -21,7 +30,6 @@ const Device = ({ conf, device, onDelete, onStop }) => {
       const sendResult = await axios.get(
         `http://localhost:3000/api/devices/${device.host}/${deviceIp.value}`
       );
-      console.log(sendResult);
       let processed = sendResult.data.filter((response) =>
         response.includes('processed: 1; failed: 0; total: 1;')
       );
@@ -54,7 +62,7 @@ const Device = ({ conf, device, onDelete, onStop }) => {
           <Button
             className="mx-3"
             variant="primary"
-            onClick={() => monitorDevice(device.host, device.hostid, deviceIp)}
+            onClick={() => monitorDevice()}
           >
             Monitor device
           </Button>
@@ -65,16 +73,32 @@ const Device = ({ conf, device, onDelete, onStop }) => {
           >
             Stop monitor
           </Button>
-          <Button
-            className="mx-3"
-            variant="danger"
-            onClick={() => onDelete(device.hostid)}
-          >
+          <Button className="mx-3" variant="danger" onClick={handleShow}>
             Delete device
           </Button>
           <Feedback message={deviceMessage} />
         </Card.Body>
       </Card>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete device from zabbix server?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          WARNING: you are deleting this device from the zabbix server. If you
+          do it you will lose all historical monitoring data. If you just want
+          this agent stop monitoring the device use the yellow 'Stop monitor'
+          button. Are you sure you want to delete the device from the server
+          instead?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            NO Cancel
+          </Button>
+          <Button variant="danger" onClick={() => onDelete(device.hostid)}>
+            YES Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
