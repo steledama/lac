c77466a52bf391519921a987bc4340007045bea6
steledama <stefano.pompa@gmail.com>
2021-10-02 22:47:39 +0200

    select menu for oid names

--- pages/components/Snmp.js
+++ pages/components/Snmp.js
@@ -6,6 +6,38 @@ const Snmp = ({ snmp, onSnmp }) => {
   const [method, setMethod] = useState('get');
   const [oid, setOid] = useState('1.3.6.1.2.1.1.1.0');
 
+  const getNames = [
+    { name: 'sysName', oid: '1.3.6.1.2.1.1.1.0' },
+    { name: 'general', oid: '1.3.6.1.2.1.1.5.0' },
+    { name: 'serial', oid: '1.3.6.1.2.1.43.5.1.1.17.1' },
+  ];
+
+  const subtreeNames = [
+    { name: 'all', oid: '1.3.6.1' },
+    { name: 'supplies', oid: '1.3.6.1.2.1.43.11.1.1' },
+    { name: 'lexamarkUsage', oid: '1.3.6.1.4.1.641.6.4.2.1.1' },
+    { name: 'xeroxUsage', oid: '1.3.6.1.4.1.253.8.53.13.2.1.6.1.20' },
+  ];
+  let selectOptions = [];
+  if (method === 'get') {
+    selectOptions = getNames.map((item, i) => {
+      return (
+        <option key={i} value={item.oid}>
+          {item.name}
+        </option>
+      );
+    });
+  }
+  if (method === 'subtree') {
+    selectOptions = subtreeNames.map((item, i) => {
+      return (
+        <option key={i} value={item.oid}>
+          {item.name}
+        </option>
+      );
+    });
+  }
+
   const onSubmit = (e) => {
     e.preventDefault();
     if (!ip) {
@@ -55,7 +87,13 @@ const Snmp = ({ snmp, onSnmp }) => {
             onChange={(e) => setMethod(e.target.value)}
           />
         </div>
-
+        <Form.Label>oid name</Form.Label>
+        <Form.Select
+          aria-label="oidNames"
+          onChange={(e) => setOid(e.target.value)}
+        >
+          {selectOptions}
+        </Form.Select>
         <Form.Label>oid</Form.Label>
         <Form.Control
           type="text"
