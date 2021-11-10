7b5a6cdd45240b748ad332d1db25e3b8ba32dbce
stefano-cla <stefano@clasrl.com>
2021-11-10 21:40:15 +0100

    working service

--- serviceInstall.js
+++ serviceInstall.js
@@ -4,9 +4,8 @@ const Service = require('node-windows').Service;
 const svc = new Service({
   name:'LAC',
   description: 'Snmp agent for monitoring remote zabbix hosts',
-  // script: 'C:\\LAC\\node_modules\\next\\dist\\bin\\next',
-  script: 'C:\\Users\\stefano\\AppData\\Roaming\\npm\\node_modules\\npm\\bin\\npm-cli.js',
-  scriptOptions: 'run dev',
+  script: 'C:\\LAC\\node_modules\\npm\\bin\\npm-cli.js',
+  scriptOptions: 'run start',
   // nodeOptions: [
   //   '--harmony',
   //   '--max_old_space_size=4096'
