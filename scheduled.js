57e5720215fcba07668eca0df0785ff559982234
stefano-cla <stefano@clasrl.com>
2021-12-05 10:51:13 +0100

    bug fix on auto update

--- scheduled.js
+++ scheduled.js
@@ -10,17 +10,23 @@ const semverGt = require('semver/functions/gt');
 
 const actualVersion = pjson.version;
 console.log(`Lac agent v${actualVersion}`);
-const latestVersion = await getLatestVersion(config.server, config.token);
-console.log(`Lac agent latest v${actualVersion}`);
 
-if (semverGt(latestVersion, actualVersion)) {
-  console.log('Must upgrade');
-  await downloadUpgrade(
-    'C:\\LAC\\win',
-    'https://github.com/steledama/lac/blob/master/win/LAC_upgrade.exe'
-  );
-  await upgradeLac();
+async function checkUpgrade() {
+  const latestVersion = await getLatestVersion(config.server, config.token);
+  console.log(`Lac agent latest v${actualVersion}`);
+
+  if (semverGt(latestVersion, actualVersion)) {
+    console.log('Must upgrade');
+    await downloadUpgrade(
+      'C:\\LAC\\win',
+      'https://github.com/steledama/lac/blob/master/win/LAC_upgrade.exe'
+    );
+    await upgradeLac();
+  } else {
+    console.log('The agent is updated');
+  }
 }
+checkUpgrade();
 
 async function monitor() {
   const result = await monitorDevices(config);
--- win/lacInstall.iss
+++ win/lacInstall.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "LAC"
-#define MyAppVersion "1.1.0"
+#define MyAppVersion "1.1.1"
 #define MyAppPublisher "steledama"
 #define MyAppURL "https://github.com/steledama/lac"
 #define user "C:\Users\stefa"
